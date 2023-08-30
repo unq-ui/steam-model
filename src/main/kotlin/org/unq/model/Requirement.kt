8fa927db910c54984530889dfb0267c203067d5a
Juan Pablo Mottesi <jmottesi@portfoliopersonal.com>
2023-08-30 20:58:40 -0300

    fix: se agregan vals a los requirements

--- src/main/kotlin/org/unq/model/Requirement.kt
+++ src/main/kotlin/org/unq/model/Requirement.kt
@@ -1,10 +1,10 @@
 package org.unq.model
 
 class Requirement(
-    os: List<String> = listOf(),
-    processor: List<String> = listOf(),
-    memory: Int = 0,
-    graphics: List<String> = listOf(),
-    directX: String = "",
-    storage: Int = 0
+    val os: List<String> = listOf(),
+    val processor: List<String> = listOf(),
+    val memory: Int = 0,
+    val graphics: List<String> = listOf(),
+    val directX: String = "",
+    val storage: Int = 0
 )
